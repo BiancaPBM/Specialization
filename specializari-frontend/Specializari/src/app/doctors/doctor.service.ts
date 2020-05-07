c136252ac197c45e07afe74542a92ef033c73e39
Bianca <pantarubianca@yahoo.com>
2020-05-07 22:11:28 +0300

    LAST ONE

---
+++ specializari-frontend/Specializari/src/app/doctors/doctor.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const baseUrl = 'http://localhost:8090/doctors';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get(baseUrl);
+  }
+
+  searchDoctor(filter) {
+    return this.http.get(baseUrl+'/filter'+filter);
+  }
+}
